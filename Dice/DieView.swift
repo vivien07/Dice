475ea2c1c21ee87efd54ae5f70f8e93b9dab09a0
Vivien ruas <vivienruas07@gmail.com>
2020-04-06 12:00:31 +0200

    Drag & Drop support Animation with timer

--- Dice/DieView.swift
+++ Dice/DieView.swift
@@ -3,6 +3,7 @@ import Cocoa
 
 class DieView: NSView {
     
+    //MARK: - Properties
     
     override var intrinsicContentSize: NSSize {
         return NSSize(width: 20, height: 20)
@@ -18,6 +19,32 @@ class DieView: NSView {
         didSet {
             needsDisplay = true
         }
+    }
+    
+    var highlighted: Bool = false {
+        didSet {
+            needsDisplay = true
+        }
+    }
+    
+    var mouseDownEvent: NSEvent?
+    var rollsRemaining = 10  //helps to determine when to stop the timer
+    
+    //MARK: - Initializers
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        commomInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commomInit()
+    }
+    
+    
+    func commomInit() {
+        self.registerForDraggedTypes([NSPasteboard.PasteboardType.string])
     }
     
     //MARK: - First Responder
@@ -49,7 +76,12 @@ class DieView: NSView {
         let bgColor = NSColor.lightGray
         bgColor.set()
         NSBezierPath.fill(bounds)
-        drawDieWithSize(size: bounds.size)
+        if highlighted {
+            let gradient = NSGradient(starting: NSColor.white, ending: bgColor)
+            gradient?.draw(in: bounds, relativeCenterPosition: NSZeroPoint)
+        } else {
+            drawDieWithSize(size: bounds.size)
+        }
         
     }
     
@@ -149,19 +181,50 @@ class DieView: NSView {
     //MARK: - Mouse Events
     
     override func mouseDown(with event: NSEvent) {
+        mouseDownEvent = event
         //checks if the user clicked in the die's view
         let dieFrame = metricsForSize(size: bounds.size).dieFrame
         let point = convert(event.locationInWindow, from: nil)
         pressed = dieFrame.contains(point)
     }
     
+    override func mouseDragged(with event: NSEvent) {
+        
+        let downPoint = mouseDownEvent!.locationInWindow
+        let dragPoint = event.locationInWindow
+        let distanceDragged = hypot(downPoint.x - dragPoint.x, downPoint.y - dragPoint.y)
+        if distanceDragged < 3 { return } //ensures there are no accidental drags
+        pressed = false
+        if let numberOfDots = numberOfDots {
+            let image = NSImage(size: bounds.size, flipped: false) { (imageBounds) -> Bool in
+                self.drawDieWithSize(size: imageBounds.size)
+                return true
+            }
+            let draggingFrameOrigin = convert(downPoint, from: nil)
+            let draggingFrame = NSRect(origin: draggingFrameOrigin, size: bounds.size)
+                .offsetBy(dx: -bounds.size.width/2, dy: -bounds.size.height/2)
+            let nsstring = String(numberOfDots) as NSString
+            let item = NSDraggingItem(pasteboardWriter: nsstring)
+            item.draggingFrame = draggingFrame
+            item.imageComponentsProvider = {
+                let component = NSDraggingImageComponent(key: .icon)
+                component.contents = image
+                component.frame = NSRect(origin: NSPoint(), size: self.bounds.size)
+                return [component]
+            }
+            beginDraggingSession(with: [item], event: mouseDownEvent!, source: self)
+        }
+        
+    }
+    
     
     override func mouseUp(with event: NSEvent) {
         if (pressed == true && event.clickCount == 2) {
-            randomize()
+            roll()
         }
         pressed = false
     }
+    
     
     func randomize() {
         numberOfDots = Int.random(in: 1...6)
@@ -245,6 +308,72 @@ class DieView: NSView {
     
     @IBAction func paste(sender: AnyObject?) {
         readFromPasteboard(pasteboard: NSPasteboard.general)
+    }
+    
+    
+    //MARK: - Timer
+    
+    func roll() {
+        Timer.scheduledTimer(timeInterval: 0.15, target: self, selector: #selector(rollTick), userInfo: nil, repeats: true)
+        window?.makeFirstResponder(nil) //removes the blue selection indicator
+    }
+    
+    @objc func rollTick(sender: Timer) {
+        randomize()
+        rollsRemaining -= 1
+        print("rollsRemaining: \(rollsRemaining)")
+        if rollsRemaining == 0 {
+            sender.invalidate()
+            rollsRemaining = 10
+            window?.makeFirstResponder(self)
+        }
+    }
+
+    
+    
+}
+
+
+extension DieView: NSDraggingSource {
+    
+    func draggingSession(_ session: NSDraggingSession, sourceOperationMaskFor context: NSDraggingContext) -> NSDragOperation {
+        return .all
+    }
+    
+   //called when the dragging session has completed.
+    func draggingSession(_ session: NSDraggingSession, endedAt screenPoint: NSPoint, operation: NSDragOperation) {
+        if operation == .delete {
+            numberOfDots = nil
+        }
+    }
+    
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        //disallows the drag if the source is the same as the destination
+        let source = sender.draggingSource as? DieView
+        if source === self {
+            return []
+        }
+        highlighted = true
+        return sender.draggingSourceOperationMask
+    }
+    
+    
+    override func draggingExited(_ sender: NSDraggingInfo?) {
+        highlighted = false
+    }
+    
+    
+    override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        return true
+    }
+    
+    
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        return readFromPasteboard(pasteboard: sender.draggingPasteboard)
+    }
+    
+    override func concludeDragOperation(_ sender: NSDraggingInfo?) {
+        highlighted = false
     }
     
     
